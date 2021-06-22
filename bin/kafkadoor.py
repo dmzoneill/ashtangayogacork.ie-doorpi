3fe7b86adcfa9624a079cca84185ec511a9da872
dmzoneill <dmz.oneill@gmail.com>
2021-06-22 16:43:18 +0100

    take initializer out of the loop

--- bin/kafkadoor.py
+++ bin/kafkadoor.py
@@ -5,18 +5,20 @@ import os
 from kafka import KafkaConsumer
 import subprocess
 
-while True:
+consumer = KafkaConsumer('door', bootstrap_servers=['46.101.231.44:9092'])
 
-    try:
-        consumer = KafkaConsumer('door', bootstrap_servers=['46.101.231.44:9092'])
+while True:
+    try:        
         for message in consumer:
             if os.path.exists("/var/www/html/scratch/enabled"):
-                print("opne door")
+                print("open door")
                 subprocess.run(["/var/www/html/bin/opendoor"])
             print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
 
         time.sleep(1)
+        print(".")
     except:
         traceback.print_exc()
         time.sleep(1)
+        consumer = KafkaConsumer('door', bootstrap_servers=['46.101.231.44:9092'])
 

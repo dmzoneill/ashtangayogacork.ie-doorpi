c4c9bf597ad9167fb721474d020dfa1b630f98f4
dmzoneill <dmz.oneill@gmail.com>
2021-06-26 09:36:11 +0100

    message output

--- door/index.php
+++ door/index.php
@@ -4,7 +4,7 @@ if(isset($_GET['id'])){
     print("opening");
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://ashtangayoga.ie/classes?door_action_applied=yes&id=" . $wpdb->insert_id . "&action=open&message=successfully opened");
+    curl_setopt($ch, CURLOPT_URL, "http://ashtangayoga.ie/classes?door_action_applied=yes&id=" . $_GET['id'] . "&action=open&message=successfully opened");
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 

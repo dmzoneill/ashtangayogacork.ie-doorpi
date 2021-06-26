b667a58a71214dc4d1f2ea85bd046b0276456f6c
dmzoneill <dmz.oneill@gmail.com>
2021-06-26 09:19:47 +0100

    callback

--- door/index.php
+++ door/index.php
@@ -1,3 +1,15 @@
 <?php
 
-shell_exec("/usr/bin/nohup /var/www/html/bin/opendoor-delayed");
+if(isset($_GET['id'])){
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://ashtangayoga.ie/classes?door_action_applied=yes&id=" . $wpdb->insert_id . "&action=open&message=successfully opened");
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+    curl_setopt($ch, CURLOPT_TIMEOUT, 3); //timeout in seconds
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    shell_exec("/usr/bin/nohup /var/www/html/bin/opendoor-delayed");
+}

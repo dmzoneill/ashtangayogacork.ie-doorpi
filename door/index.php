4e7947ce17380c9e116bd276a240358005fd659a
dmzoneill <dmz.oneill@gmail.com>
2021-06-26 10:29:40 +0100

    nohup

--- door/index.php
+++ door/index.php
@@ -6,7 +6,7 @@ if(isset($_GET['id'])){
     $url = "https://ashtangayoga.ie/classes/?action=door_action_applied&id=" . $_GET['id'] . "&daction=open&dmessage=successfully%20opened";
     print($url . "\n");
     
-    shell_exec("/usr/bin/curl \"$url\"");
+    shell_exec("/usr/bin/nohup /usr/bin/curl \"$url\"");
     shell_exec("/usr/bin/nohup /var/www/html/bin/opendoor-delayed");
     print(".");
 } else {

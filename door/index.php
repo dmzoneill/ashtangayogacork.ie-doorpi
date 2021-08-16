221fbba856c0f895d8391a289aa8fa9bd1d41248
dmzoneill <dmz.oneill@gmail.com>
2021-08-16 19:50:21 +0100

    local updates

--- door/index.php
+++ door/index.php
@@ -5,9 +5,10 @@ if(isset($_GET['id'])){
 
     $url = "https://ashtangayoga.ie/classes/?action=door_action_applied&id=" . $_GET['id'] . "&daction=open&dmessage=successfully%20opened";
     print($url . "\n");
-    
-    shell_exec("/usr/bin/nohup /usr/bin/curl \"$url\"");
-    shell_exec("/usr/bin/nohup /var/www/html/bin/opendoor-delayed");
+
+    echo shell_exec("sudo whoami"); 
+    echo shell_exec("sudo /var/www/html/bin/opendoor-delayed");
+    echo shell_exec("sudo /usr/bin/curl \"$url\"");
     print(".");
 } else {
     print("not opening\n");

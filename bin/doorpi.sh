44eeb69c7cd1c691c4a830dbe934c9ad318c162a
dmzoneill <dmz.oneill@gmail.com>
2021-08-18 19:41:32 +0100

    cleanup

--- bin/doorpi.sh
+++ bin/doorpi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 15
 /var/www/html/bin/startvpn.sh
+chromium-browser --start-fullscreen http://localhost &
 sleep 25
 sudo nohup /usr/bin/python3 /var/www/html/shala-manager.py > /tmp/shala &
 sudo nohup /usr/bin/python3 /var/www/html/bin/keepalive.py &
 sudo nohup /usr/bin/python3 /var/www/html/bin/kafkadoor.py &
 (cd /var/www/html/heating; sudo nohup uvicorn --host 0.0.0.0 heater:app)
-sleep 10
-chromium-browser --start-fullscreen http://localhost &
+
 

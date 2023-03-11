ce601ec7e2412a7cb2b9de1a82d80520e6859369
David O Neill <dmz.oneill@gmail.com>
2023-03-11 20:31:39 +0000

    fix protocol

--- js/doorpi.js
+++ js/doorpi.js
@@ -2,18 +2,18 @@ boost_running = false;
 boost_timer = null;
 boost_count = 900;
 
-var host = window.location.host; 
+var host = window.location.host;
 
 function update_schedule() {
-  $.get("http://" + host + "/index.php?schedule=true", function (data) {
+  $.get(document.location.protocol + "//" + host + "/index.php?schedule=true", function (data) {
     $("#schedule").show();
     $("#scheduletable").html(data);
     heating_controls();
-    $.get("http://" + host + "/scratch/temperature", function (data) {
-      $("#metric_temp").text(data + "C");    
+    $.get(document.location.protocol + "//" + host + "/scratch/temperature", function (data) {
+      $("#metric_temp").text(data + "C");
     });
-    $.get("http://" + host + "/scratch/humidity", function (data) {
-      $("#metric_humid").text(data + "%");    
+    $.get(document.location.protocol + "//" + host + "/scratch/humidity", function (data) {
+      $("#metric_humid").text(data + "%");
     });
   });
 }
@@ -30,25 +30,23 @@ function counterdown() {
 
   mins = Math.floor(boost_count / 60);
   secs = boost_count % 60;
-  if( secs % 3 > 0 )
-  {
+  if (secs % 3 > 0) {
     $("#schedule_boost").text(mins + " mins " + secs + " secs");
   }
-  else
-  {
+  else {
     $("#schedule_boost").text("Reset heaters");
   }
 }
 
-function heating_controls() {  
+function heating_controls() {
   $("#schedule_boost").click(function () {
     if (boost_running == false) {
       boost_timer = setInterval(counterdown, 1000);
       boost_running = true;
-      $.get("http://" + host + ":8000/boost");
+      $.get(document.location.protocol + "//" + host + ":8000/boost");
     }
     else {
-      $.get("http://" + host + ":8000/reset");
+      $.get(document.location.protocol + "//" + host + ":8000/reset");
       boost_running = false;
       clearInterval(boost_timer);
       $("#schedule_boost").text("15 Mins");
@@ -58,7 +56,7 @@ function heating_controls() {
 }
 
 function opendoor() {
-  $.get("http://" + host + "/index.php?opendoor=true");
+  $.get(document.location.protocol + "//" + host + "/index.php?opendoor=true");
 }
 
 $(document).ready(function () {
